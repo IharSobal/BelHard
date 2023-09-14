d35fee189c3e641c166dbac99664c0ababc7686a
IharSobal <igor.djoker@gmail.com>
2023-09-14 20:21:57 +0300

    ready

--- 3_1.py
+++ 3_1.py
@@ -1,5 +1,7 @@
 # text = input("Введите предложение:")
 text = "Добро пожаловать в Минск!"
-a = (text.split(' '))
+a = text.split(' ')
 text2 = '-'.join(a)
 print(text2)
+text3 = text.replace(' ', '-')
+print(text3)
