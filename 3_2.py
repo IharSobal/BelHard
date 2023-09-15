93d9e5673501225c3d38bbde0ed90030e7edce79
IharSobal <igor.djoker@gmail.com>
2023-09-15 13:51:59 +0300

    ready_2

--- 3_2.py
+++ 3_2.py
@@ -5,3 +5,9 @@ d = (a + b + c) / 3
 print("Среднее арифметическое с точностью 3 =", round(d, 3))
 text = f"{d:.3f}"
 print("Среднее арифметическое с точностью 3 =", text)
+total = 0
+for _ in range(3):
+    num = float(input("Введите число: "))
+    total += num
+average = total/3
+print("Среднее арифметическое с точностью 3 =", round(average, 3))
