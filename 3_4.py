794d6aba1a8bc70e6dfb6e1cd349320888ee21ad
IharSobal <igor.djoker@gmail.com>
2023-09-15 13:30:48 +0300

    ready_2

--- 3_4.py
+++ 3_4.py
@@ -1,19 +1,10 @@
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
-c = float(input("Введите третье число: "))
 x = 0
 y = 0
-if a >= 0:
-    x += 1
-else:
-    y +=1
-if b >= 0:
-    x += 1
-else:
-    y +=1
-if c >= 0:
-    x += 1
-else:
-    y +=1
+for _ in range(3):
+    num = float(input("Введите число: "))
+    if num >= 0:
+        x += 1
+    else:
+        y += 1
 print("Положительных чисел =", x)
 print("Отрицательных чисел =", y)
