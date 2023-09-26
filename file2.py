1240b8e7e8166885b4842714a08ccd9b12be0ee9
IharSobal <igor.djoker@gmail.com>
2023-09-26 19:41:01 +0300

    lesson 6

--- file2.py
+++ file2.py
@@ -1,4 +1,8 @@
 # text = input('Введите возраст ')
 # while not text.isdigit():
 #     text = input('Введите возраст числом ')
-sum_vklad = float(input())
+# sum_vklad = float(input())
+# def is_palindrom(a):
+#     a = input()
+#     b = reversed(a)
+#     if
