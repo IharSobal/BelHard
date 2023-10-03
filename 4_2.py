b7f8f63e584eefdce83c8b0711cc5458f0464979
IharSobal <igor.djoker@gmail.com>
2023-10-03 16:29:15 +0300

    ready

--- 4_2.py
+++ 4_2.py
@@ -1,14 +1,16 @@
 #TODO Без использования collections, написать программу, которая будет создавать
 # словарь для подсчитывания количества вхождения каждой буквы в текст введенный с клавиатуры
-#text = input()
-text = 'Доброго дня, родной город Минск, и всего наилучшего!'
+text = input()
+#text = 'Доброго дня, родной город Минск, и всего наилучшего!'
 text = text.lower()
 some_list = list(text)
-some_tuple = tuple(text)
-for i in range(len(some_list)):
-    print(some_list.count(i))
-
-#some_dict =
+some_dict = {}
+#some_tuple = tuple(text)
+for i in some_list:
+    some_dict_letter = {i: some_list.count(i)}
+    if i.isalpha():
+        some_dict.update(some_dict_letter)
+print(some_dict)
 # print(some_list)
 # print(some_tuple)
 #print(some_list.count('д'))
