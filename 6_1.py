6ae4aed1dd23a726a7324a8f2f4da0ec8128c43a
IharSobal <igor.djoker@gmail.com>
2023-10-13 21:12:00 +0300

    ready

--- 6_1.py
+++ 6_1.py
@@ -1,2 +1,10 @@
 #TODO Написать функцию перевода десятичного числа в двоичное и обратно,
 # без использования функции int
+def convert_10_2_10(decimal_num):
+    bin_num = f'{decimal_num:b}'
+    dec_num = eval('0b' + bin_num)
+    return dec_num
+
+n = 2023
+
+print(convert_10_2_10(n))
