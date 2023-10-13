37a669dae7e2d3a00103d40209573ed2782ba19d
IharSobal <igor.djoker@gmail.com>
2023-10-13 18:35:56 +0300

    ready

--- 5_1.py
+++ 5_1.py
@@ -1,1 +1,8 @@
-#TODO Вывести первые N цисел кратные M и больше K
+#TODO Вывести первые N чисел кратные M и больше K
+n, m, k = int(input()), int(input()), int(input())
+counter = 0
+while counter < n:
+    k += 1
+    if not k % m:
+        print(k, end=' ')
+        counter += 1
