1542c4ca7337b42dd783f1f92c8a6dda1eb48720
IharSobal <igor.djoker@gmail.com>
2023-10-13 17:11:10 +0300

    ready

--- 5_3.py
+++ 5_3.py
@@ -1,1 +1,10 @@
 #TODO Вывести четные числа от 2 до N по 5 в строку
+n = int(input())
+counter = 0
+for i in range(2, n + 1):
+    if not i % 2:
+        counter += 1
+        print(i, end=' ')
+        if counter == 5:
+            print('')
+            counter = 0
