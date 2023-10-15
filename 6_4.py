97ea0b42a118d53f9715e4d6730164db091820a6
IharSobal <igor.djoker@gmail.com>
2023-10-15 15:33:50 +0300

    ready

--- 6_4.py
+++ 6_4.py
@@ -1,2 +1,10 @@
 #TODO Дан список содержащий в себе различные типы данных, отфильтровать таким
 # образом, чтобы остались только строки, использование дополнительного списка незаконно
+s = [1, 2, 3, 4, 5, 'a', 'b', 'c', 6, 7, 8, 'ab', 'bc', 'ac', 9, 10]
+def filter_str(in_list):
+    if type(in_list) is str:
+        return True
+    else:
+        return False
+s = [*filter(filter_str, s)]
+print(s)
