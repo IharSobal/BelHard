85cd7b62a8ab304c9e2c6e2e84ccc90f0d3f164c
IharSobal <igor.djoker@gmail.com>
2023-10-16 10:45:55 +0300

    ready

--- 6_8.py
+++ 6_8.py
@@ -1,2 +1,14 @@
 #TODO Дан словарь, ключ - Название страны, значение - список городов,
 # на вход поступает город, необходимо сказать из какой он страны
+d = {
+    'Belarus': ['Minsk', 'Brest', 'Vitebsk', 'Grodno', 'Gomel', 'Mogilev'],
+    'Russia': ['Moscow', 'Saint Petersburg', 'Pskov', 'Novgorod'],
+    'Germany': ['Berlin', 'Hamburg', 'Mun-hen', 'Bзначению phpremen'],
+    'France': ['Paris', 'Bordo', 'Lion', 'Nica', 'Marsel']
+}
+city = 'Hamburg'
+for i in d.keys():
+    for j in d[i]:
+        if city.lower() == j.lower():
+            print(i)
+            break
