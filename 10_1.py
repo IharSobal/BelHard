8ecddffe8e89e0ab1dfc0005162046726c43e617
IharSobal <igor.djoker@gmail.com>
2023-10-16 18:31:23 +0300

    test

--- 10_1.py
+++ 10_1.py
@@ -1,2 +1,4 @@
 #TODO Написать класс метод from_csv принимающий объект file (csv),
 # и возвращающий список экземпляров схемы заполненных на основании этого файла
+from pydantic import BaseModel
+import csv
