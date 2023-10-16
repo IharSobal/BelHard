65998db859389d0747fd00e9bb9b1c41db393029
IharSobal <igor.djoker@gmail.com>
2023-10-16 08:32:09 +0300

    ready

--- 6_5.py
+++ 6_5.py
@@ -1,7 +1,7 @@
 #TODO Дан список чисел, необходимо его развернуть без использования
 # метода reverse и функции reversed, а так же дополнительного списка и среза
 s = [1, 2, 3, 4, 5, 6, 7]
-for i in range(len(s)):
-    for j in range(-1, -len(s)-1, -1):
-        s[i] = s[j]
+for i in range(len(s) // 2):
+    s[i], s[-i -1] = s[-i -1], s[i]
 print(s)
+
